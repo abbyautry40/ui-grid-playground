ff9db3fb202391d37fd1c8d3032f841fbd2c7843
Abby Autry <abby.autry@fortyau.com>
2022-06-10 14:03:06 -0500

    Grid setup

--- app/app.module.js
+++ app/app.module.js
@@ -1,10 +1,5 @@
-'use strict';
-
 // Define the `phonecatApp` module
 angular.module('phonecatApp', [
-  'ngAnimate',
-  'ngRoute',
-  'core',
-  'phoneDetail',
-  'phoneList'
-]);
+    // ...which depends on the `phoneList` module
+    'phoneList'
+  ]);
--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,14 +1,29 @@
-'use strict';
-
 // Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
+angular
+  .module('phoneList')
+  .component('phoneList', {  // This name is what AngularJS uses to match to the `<phone-list>` element.
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
-        this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
+    controller: ['$http', '$scope',
+      function PhoneListController($http, $scope) {
+        $scope.phones = [];
+
+        $scope.gridOptions = {
+          data: [],
+          enableGridMenu: true,
+          enableFiltering: true,
+          onRegisterApi: function (gridApi) {
+            $scope.gridApi = gridApi;
+        },
+        }
+
+        $http.get('/phones/phones.json').then(function (response) {
+          $scope.phones = response.data;
+          $scope.gridOptions.data = $scope.phones;
+        });
+
+        $scope.clear = function() {
+          $scope.gridApi.core.clearAllFilters();
+          // console.log($scope.gridApi);
+      };
+      }]
   });
